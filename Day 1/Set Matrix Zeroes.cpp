7ed2f2c663d15c2cf51e8263ee0eb06f1481ffe8
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-06-08 21:23:26 -0400

    Create Set Matrix Zeroes.cpp

---
+++ Day 1/Set Matrix Zeroes.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h> 
+void setZeros(vector<vector<int>> &matrix)
+{
+    bool firstRow = false, firstCol = false;
+    int n = matrix.size(), m = matrix[0].size();
+    for(int i = 0; i < n; i++) {
+        if(matrix[i][0] == 0) {
+            firstCol = true;
+        }
+    }
+    for(int i = 0; i < m; i++) {
+        if(matrix[0][i] == 0) {
+            firstRow = true;
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(matrix[i][j] == 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+    for(int i = 1; i < n; i++) {
+        if(matrix[i][0] == 0) {
+            for(int j = 1; j < m; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    
+    for(int i = 1; i < m; i++) {
+        if(matrix[0][i] == 0) {
+            for(int j = 1; j < n; j++) {
+                matrix[j][i] = 0;
+            }
+        }
+    }
+    
+    if(firstCol) {
+        for(int i = 0; i < n; i++) {
+            matrix[i][0] = 0; 
+        }
+    }
+    
+    if(firstRow) {
+        for(int i = 0; i < m; i++) {
+            matrix[0][i] = 0;
+        }
+    }
+}
