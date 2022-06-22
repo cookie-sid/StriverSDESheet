cda1f52d04950ff8537505a989b5dc46e4703a37
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-06-21 22:51:25 -0400

    Update 2-Sum-Problem.cpp

--- Day 4/2-Sum-Problem.cpp
+++ Day 4/2-Sum-Problem.cpp
@@ -13,3 +13,21 @@ vector<vector<int>> pairSum(vector<int> &arr, int s){
     sort(ans.begin(),ans.end());
     return ans;
 }
+
+//LC
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> m;
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(m.find(target-nums[i]) != m.end())
+                return {m[target - nums[i]],i};
+            
+            m[nums[i]] = i;
+        }
+        return {0,0};
+    }
+};
