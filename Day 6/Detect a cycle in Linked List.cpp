44e7fe02c2e0ad3f1bdc29d516c33288e3968ae9
cookie-sid <95382731+cookie-sid@users.noreply.github.com>
2022-07-06 16:25:01 -0400

    Create Detect a cycle in Linked List.cpp

---
+++ Day 6/Detect a cycle in Linked List.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL) {
+            return false;
+        }
+        ListNode *slow = head, *fast = head;
+        while(fast -> next != NULL) {
+            slow = slow -> next;
+            fast = fast -> next;
+            if(fast -> next == NULL) {
+                return false;
+            }
+            fast = fast -> next;
+            if(slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
